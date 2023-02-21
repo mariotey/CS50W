c58d79a96cd1dc673837cd52cbbe5d479eff6acc
mctey <tey.mingchuan@yahoo.com.sg>
2023-02-21 22:26:53 +0800

    Updated pathways

--- Project_1/encyclopedia/urls.py
+++ Project_1/encyclopedia/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),   
     path("<str:name>", views.entry, name="entry"),
-    path("get_result/", views.get_result, name="get_result")
+    path("get_result/", views.get_result, name="get_result"),
+    path("new_result/", views.new_result, name="new_result"),
+    path("save_result/", views.save_result, name="save_result"),
+    path("save_edit/", views.save_edit, name="save_edit"),
+    path("edit_result/", views.edit_result, name="edit_result"),    
+    path("random_result/", views.random_result, name="random_result")
 ]
--- Project_1/encyclopedia/views.py
+++ Project_1/encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django import forms
 from . import util
+import markdown as md
+from markupsafe import Markup
+import random
 
-# class NewTaskForm(forms.Form):
-#     task = forms.CharField(label="New Task")
+class NewResultTitle(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Title', 'size': '177'}))
+
+class NewResultText(forms.Form):
+    textarea = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': 'Description'}))
+
+class NoEditTitle(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Title', 'size': '177', 'readonly':'readonly'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -14,18 +22,75 @@ def index(request):
 def entry(request, name):
     return render(request, "encyclopedia/entry.html", {
         "entries": util.list_entries(),
-        "entry_content": util.get_entry(name)
+        "entry_content": Markup(md.markdown(util.get_entry(name)))
     })
 
 def get_result(request):
-    return entry(request, "css")
+    query = request.POST["q"]
 
-# def add(request):
-#     if request.method == "POST":
-#         form = NewTaskForm(request.POST)
-#         if form.is_valid():
-#             task = form.cleaned_data["task"]
+    list_entries = util.list_entries()
 
-#     return render(request, "tasks/add.html", {
-#         "form": NewTaskForm()
-#     })
+    if query.lower() in [x.lower() for x in list_entries]:
+        return redirect(f"/wiki/{query}")
+    else:
+        new_listentries = [entry for entry in list_entries if query in entry]
+        if new_listentries:
+            return render(request, "encyclopedia/index.html", {
+                "entries": new_listentries
+            })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Requested page is not found!"
+            })
+    
+def new_result(request):
+    new_title = NewResultTitle()
+    new_text = NewResultText()
+
+    return render(request, "encyclopedia/newpage.html", {
+        "title": new_title,
+        "textarea": new_text
+    })
+
+def save_result(request):
+    query = request.POST["title"]
+    list_entries = util.list_entries()
+    
+    if query.lower() in [x.lower() for x in list_entries]:    
+         return render(request, "encyclopedia/error.html", {
+                "message": "Requested page has already been created!"
+            })
+    else:
+        with open(f"entries/{query}.md", "w") as f:
+            f.write("#" + request.POST["title"])
+            f.write("\n\n")
+            f.write(request.POST["textarea"])
+
+        return redirect(f"/wiki/{query}")
+    
+def save_edit(request):
+    query = request.POST["title"]
+
+    with open(f"entries/{query}.md", "w") as f:
+        f.write(request.POST["textarea"])
+
+    return redirect(f"/wiki/{query}")
+
+def edit_result(request):
+    last_url = request.META.get('HTTP_REFERER')
+    url_exten = last_url.split("/wiki/", 1)[1]
+    
+    new_title = NoEditTitle({"title": url_exten.lower()})
+    new_text = NewResultText({"textarea": md.markdown(util.get_entry(url_exten))})
+
+    return render(request, "encyclopedia/editpage.html", {
+        "title": new_title,
+        "textarea": new_text
+    })
+
+
+def random_result(request):
+    list_entries = util.list_entries()
+    query = random.choice(list_entries)
+
+    return redirect(f"/wiki/{query}")
