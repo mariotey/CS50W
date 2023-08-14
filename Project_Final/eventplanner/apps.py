2928193c8865388e6dba57ce74bbd05e4d5ad446
mctey <tey.mingchuan@yahoo.com.sg>
2023-08-14 23:43:46 +0800

    Added Timetable

--- Project_Final/eventplanner/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventplannerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eventplanner'
--- Project_Final/timetable/views.py
+++ Project_Final/timetable/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def index(request):
+    return render(request, "timetable/index.html")
